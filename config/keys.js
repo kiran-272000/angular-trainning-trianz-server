bb67410b0eeb580e21993039fff4060d2ea253a0
kiran-272000 <vellingirikiran@gmail.com>
2022-09-12 20:16:15 +0530

    initial

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://kiran:dGwzqaox9eYE7dDF@ac-vqj21jw-shard-00-00.y0dpuzi.mongodb.net:27017,ac-vqj21jw-shard-00-01.y0dpuzi.mongodb.net:27017,ac-vqj21jw-shard-00-02.y0dpuzi.mongodb.net:27017/devconnector?ssl=true&replicaSet=atlas-pa2xin-shard-0&authSource=admin&retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
